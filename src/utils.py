ebd0a590256f1e111cab49b24686b21f25176aaf
Ahmed <ahmed.mca18@gmail.com>
2024-01-08 13:29:25 +0530

    added notebook

--- src/utils.py
+++ src/utils.py
@@ -30,7 +30,7 @@ def read_sql_data():
             db=db
         )
         logging.info("Connection Established",mydb)
-        df=pd.read_sql_query('Select * from survey_results_public',mydb)
+        df=pd.read_sql_query('Select * from survey',mydb)
         print(df.head())
 
         return df
