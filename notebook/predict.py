f45821836e5691b6230d240a2159e6a0b84d4a7a
Ahmedullah (ahmed) <109277143+AgentAhmed@users.noreply.github.com>
2024-01-15 22:43:25 +0530

    Update predict.py

--- notebook/predict.py
+++ notebook/predict.py
@@ -44,7 +44,7 @@ def show_predict_page():
         "Sweden",
     )
 
-    education_levels = (
+    education = (
         "Less than a Bachelors",
         "Bachelor’s degree",
         "Master’s degree",
@@ -52,37 +52,16 @@ def show_predict_page():
     )
 
     country = st.selectbox("Country", countries)
-    education = st.selectbox("Education Level", education_levels)
+    education = st.selectbox("Education Level", education)
 
-    experience = st.slider("Years of Experience", 0, 50, 3)
+    expericence = st.slider("Years of Experience", 0, 50, 3)
 
     ok = st.button("Calculate Salary")
     if ok:
-        X = np.array([[country, education, experience]])
+        X = np.array([[country, education, expericence ]])
         X[:, 0] = le_country.transform(X[:,0])
         X[:, 1] = le_education.transform(X[:,1])
         X = X.astype(float)
 
         salary = regressor.predict(X)
-        st.subheader(f"The estimated salary is ${salary[0]:,.2f}")
-
-        # Display additional visualizations
-        st.write(
-            """
-            ### Additional Visualizations
-            """
-        )
-
-        # Bar chart showing average salary based on education level
-        education_data = pd.DataFrame({"Education Level": education_levels})
-        education_data["Average Salary"] = education_data["Education Level"].apply(
-            lambda x: regressor.predict(np.array([[0, le_education.transform(x), 3]]))[0]
-        )
-        st.bar_chart(education_data.set_index("Education Level")["Average Salary"])
-
-        # Line chart showing salary distribution across different countries
-        countries_data = pd.DataFrame({"Country": countries})
-        countries_data["Average Salary"] = countries_data["Country"].apply(
-            lambda x: regressor.predict(np.array([[le_country.transform(x), 0, 3]]))[0]
-        )
-        st.line_chart(countries_data.set_index("Country")["Average Salary"])
+        st.subheader(f"The estimated salary is ${salary[0]:.2f}")
